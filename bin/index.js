23375ea7973c3dbf7f43c0725a6ee43ac53f6cc5
joyffp <joyffp@qq.com>
2020-07-29 13:58:03 +0800

    name -> dq

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 const photo = require("../index.js")
 ;(async () => {
     if (process.argv[2] == "photo") return photo()
+    if (process.argv[2] == "-p") return photo()
     return help()
 })()
 
@@ -11,4 +12,5 @@ function help() {
     console.info("")
     console.info("Commands:")
     console.info("  photo                          变更文件名称")
+    console.info("  -p                             变更文件名称")
 }
