176f1cc0abf7f2daeed9f8786245f082e0a04aa2
joyffp <joyffp@qq.com>
2020-07-29 14:17:37 +0800

    去掉简写命令

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,7 @@ const photo = require("../core/photo.js")
 const dqHttp = require("../core/http.js")
 ;(async () => {
     if (process.argv[2] == "photo") return photo()
-    if (process.argv[2] == "-p") return photo()
     if (process.argv[2] == "http") return dqHttp()
-    if (process.argv[2] == "-h") return dqHttp()
     return help()
 })()
 
@@ -15,7 +13,5 @@ function help() {
     console.info("")
     console.info("Commands:")
     console.info("  photo                          变更文件名称")
-    console.info("  -p                             变更文件名称")
     console.info("  http                           启动HTTP服务")
-    console.info("  -h                             启动HTTP服务")
 }
