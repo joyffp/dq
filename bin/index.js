00070ba8eb32dbac94cc9f04a4572b67281ec8ed
fengfupeng <fengfupeng@xdf.cn>
2020-08-06 12:02:50 +0800

    add ssh

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const photo = require("../core/photo.js")
 const dqHttp = require("../core/http.js")
+const dqSSH = require("../core/ssh.js")
 ;(async () => {
     if (process.argv[2] == "photo") return photo()
     if (process.argv[2] == "http") return dqHttp()
+    if (process.argv[2] == "ssh") return dqSSH()
     return help()
 })()
 
@@ -14,4 +16,5 @@ function help() {
     console.info("Commands:")
     console.info("  photo                          变更文件名称")
     console.info("  http                           启动HTTP服务")
+    console.info("  ssh                            启动SSH服务")
 }
---
+++ core/ssh.js
@@ -0,0 +1,5 @@
+// 2020-08-06 11:50:42 init
+function dqSSH() {
+    console.log(`node "/Users/fengfupeng/.vscode/extensions/ruakr.ftp-kr-1.3.16/out/tool/ssh.js" "${process.cwd()}" 0`)
+}
+module.exports = dqSSH
